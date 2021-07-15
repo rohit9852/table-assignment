cd0aeaa664a11214aa844069595f66a75538c27b
rohit9852 <rohit9852@gmail.com>
2021-07-15 11:48:36 +0530

    make pagination

---
+++ src/component/PaginationTable.js
@@ -0,0 +1,109 @@
+import React, { useMemo } from "react";
+import { useTable, usePagination } from "react-table";
+import MOCK_DATA from "../data/MOCK_DATA.json";
+import { Columns } from "./columns";
+import "./table.css";
+
+export const PaginationTable = () => {
+  const columns = useMemo(() => Columns, []);
+  const data = useMemo(() => MOCK_DATA.shipments, []);
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    previousPage,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    state,
+    gotoPage,
+    pageCount,
+    setPageSize,
+    prepareRow
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 }
+    },
+    usePagination
+  );
+
+  const { pageIndex, pageSize } = state;
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>{" "}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>{" "}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>{" "}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>{" "}
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <span>
+          | Go to page:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: "50px" }}
+          />
+        </span>{" "}
+        <select
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[5, 10, 15, 20, 25, 30, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
+};
---
+++ src/component/columns.js
@@ -0,0 +1,57 @@
+export const Columns = [
+  {
+    Header: "Id",
+    Footer: "Id",
+    accessor: "id",
+    disableFilters: true,
+    sticky: "left"
+  },
+  {
+    Header: "Name",
+    Footer: "Name",
+    accessor: "name",
+    sticky: "left"
+  },
+  {
+    Header: "Mode",
+    Footer: "Mode",
+    accessor: "mode",
+    sticky: "left"
+  },
+  {
+    Header: "Type",
+    Footer: "Type",
+    accessor: "type",
+    sticky: "left"
+  },
+  {
+    Header: "Destination",
+    Footer: "Destination",
+    accessor: "destination",
+    sticky: "left"
+  },
+  {
+    Header: "Origin",
+    Footer: "Origin",
+    accessor: "origin",
+    sticky: "left"
+  },
+  {
+    Header: "Total",
+    Footer: "Total",
+    accessor: "total",
+    sticky: "left"
+  },
+  {
+    Header: "Status",
+    Footer: "Status",
+    accessor: "status",
+    sticky: "left"
+  },
+  {
+    Header: "userId",
+    Footer: "userId",
+    accessor: "userId",
+    sticky: "left"
+  }
+];
